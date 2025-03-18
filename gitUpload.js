7d4795317cf9eb78de9cb9395c1fad361cfebd1b
spospordo <johnhowieson10@gmail.com>
2025-03-17 21:45:54 -0700

    Update gitUpload.js

--- gitUpload.js
+++ gitUpload.js
@@ -3,13 +3,14 @@ const simpleGit = require('simple-git');
 const fs = require('fs');
 const path = require('path');
 const { gitConfig } = require('./config'); // Import the configuration
+const fsExtra = require('fs-extra'); // To copy files
 
 // Initialize simple-git with the path to your local git repo
 const git = simpleGit();
 
 // Function to upload the file to the Git repo
 async function uploadFile() {
-  const { username, password, repoURL, folderPath, commitMessage, fileToUpload, branchName } = gitConfig;
+  const { username, password, repoURL, folderPath, commitMessage, fileToUpload, targetFolder, branchName } = gitConfig;
 
   try {
     // Change to the folder where the git repository is located (if it's not already initialized)
@@ -26,9 +27,16 @@ async function uploadFile() {
     // Checkout to the specific branch if it's not already checked out
     await git.checkout(branchName);
 
-    // Add the file to the staging area
+    // Define the target file path where you want to copy the file in the repo
+    const targetPath = path.join(repoPath, targetFolder, path.basename(fileToUpload));
+    
+    // Copy the file to the target folder in the repository
+    console.log(`Copying file to ${targetPath}...`);
+    await fsExtra.copy(fileToUpload, targetPath);
+
+    // Add the copied file to the git staging area
     console.log('Adding file to git...');
-    await git.add(fileToUpload);
+    await git.add(path.join(targetFolder, path.basename(fileToUpload)));
 
     // Commit the changes
     console.log('Committing the changes...');
